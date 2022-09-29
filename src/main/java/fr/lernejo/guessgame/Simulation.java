b74f9d3ef44da135e04108899b804891ff9db882
Augustin Ponsin <augustin.ponsin@epita.fr>
2022-09-29 17:03:17 +0200

    Simplification des messages de log

--- src/main/java/fr/lernejo/guessgame/Simulation.java
+++ src/main/java/fr/lernejo/guessgame/Simulation.java
@@ -1,17 +1,20 @@
 package fr.lernejo.guessgame;
-
 import fr.lernejo.logger.Logger;
 import fr.lernejo.logger.LoggerFactory;
 
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Simulation {
     private final Logger logger = LoggerFactory.getLogger("simulation");
-    private final Player player;
-    private long numberToGuess;
-
+    private final Player player;  //TODO add variable type
+    private long numberToGuess; //TODO add variable type
     public Simulation(Player player) {
         this.player = player;
     }
-
     public void initialize(long numberToGuess) {
         this.numberToGuess = numberToGuess;
     }
@@ -30,10 +33,21 @@ public class Simulation {
         return true;
     }
 
-    public void loopUntilPlayerSucceed() {
-        boolean done = false;
-        while (!done) {
-            done = nextRound();
+    public void loopUntilPlayerSucceed(long limit) {
+        boolean isFinished = false;
+        long n = 0;
+        long startTime = System.currentTimeMillis();
+        while (!isFinished && n < limit){
+            isFinished = nextRound();
+            n++;
         }
+        long durringTime = System.currentTimeMillis() - startTime;
+        Date time = new Date(durringTime);
+        DateFormat df = new SimpleDateFormat("mm:ss.SSS");
+        logger.log("During time: " + df.format(time));
+        if (isFinished)
+            logger.log("Player has found the number in " + n + " iterations");
+        else
+            logger.log("Player has not found the number within " + limit + " iterations");
     }
 }
